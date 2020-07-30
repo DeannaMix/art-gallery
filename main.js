2cec9b743b8356696eed94d09900e2d30bd213da
Deanna Mix <deanna@LAPTOP-PG68556H.localdomain>
2020-07-29 22:50:00 -0500

    Added files and bootsrap

---
+++ main.js
@@ -0,0 +1,14 @@
+const paintings = [
+    {
+      artist: "Van Gogh",
+      imgUrl: "https://cdn.britannica.com/78/43678-050-F4DC8D93/Starry-Night-canvas-Vincent-van-Gogh-New-1889.jpg",
+      name: "Starry Night"
+    },
+    {
+      artist: "Monet",
+      imgUrl: "https://www.christies.com/img/LotImages/2019/NYR/2019_NYR_17658_0012A_000(claude_monet_le_train_a_jeufosse).jpg",
+      name: "Le train à Jeufosse"
+    },
+  ];
+
+  
